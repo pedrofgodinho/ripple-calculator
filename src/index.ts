9e63114d77b1e76c713206f1b76eda5454b6696a
Pedro Godinho <pedro.fernandez.godinho@gmail.com>
2024-06-18 16:23:36 +0100

    Add missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 
-export { BaseStats, MoveElement, Move, MoveType, StatType, CharacterStats, Stat } from "./stats";
+export { BaseStats, MoveElement, Move, MoveType, StatType, CharacterStats, Stat, StackableStat } from "./stats";
 export { Echo } from "./echoes";
+export { Weapon } from "./weapons";
+export { Character } from "./characters";
